3eed624104338cfc7e67388aca0276c38f5b7cf5
Kiminod_96 <dominik.j.malek@gmail.com>
2024-12-01 00:03:49 +0100

    First pages
    Adding main and not-found pages
    Installing bootstrap
    Adding routing to project
    
    Co-Authored-By: Maciej Maniakowski <141336189+ElMaccio@users.noreply.github.com>

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { CardComponent } from "../card/card.component";
+import { NgFor } from '@angular/common';
+import {HeaderComponent} from '../header/header.component';
+import {FooterComponent} from '../footer/footer.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CardComponent, NgFor, HeaderComponent, FooterComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  projects = [
+    {name: "Project 1", description: "Project 1 description"},
+    {name: "Project 2", description: "Project 2 description"},
+    {name: "Project 3", description: "Project 3 description"},
+    {name: "Project 4", description: "Project 4 description"},
+    {name: "Project 5", description: "Project 5 description"}
+  ];
+}
