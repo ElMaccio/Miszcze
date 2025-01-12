9bc1261f281b7fa0442f7a4715999f5d726bd122
ElMaccio <maciej.maniakowskii@gmail.com>
2025-01-12 13:18:26 +0100

    Added dark theme to the page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'backgroundPattern': "url('/topography.svg'), linear-gradient(to top right, #22c55e, #22c55e, #0d9488, #4f46e5)"
-      }
     },
   },
   plugins: [],
